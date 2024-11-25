965a340dfab840ad0191cdf1cb0fa2d1eae01dda
Guilherme Tognetti <email.guilherme.t@gmail.com>
2024-11-25 16:39:30 -0300

    added size system

--- lib/entview.dart
+++ lib/entview.dart
@@ -10,7 +10,9 @@ abstract class EntView<T extends Ent> extends StatelessWidget {
   final ValueNotifier listenable = ValueNotifier(false);
   final T get;
 
-  EntView(this.get, {super.key});
+  EntView(this.get, {super.key}) {
+    get["_initialized"] = false;
+  }
 
   sync(List<String> props) {
     for (final prop in props) {
@@ -20,14 +22,42 @@ abstract class EntView<T extends Ent> extends StatelessWidget {
     }
   }
 
+  double height(double percentage) =>
+      (get["height"] ?? 100.0) / 100.0 * percentage;
+  double width(double percentage) =>
+      (get["width"] ?? 100.0) / 100.0 * percentage;
+
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
         listenable: listenable,
         builder: (context, _) {
-          return render(context);
+          return LayoutBuilder(builder: (context, _) {
+            if (!get["_initialized"]) {
+              _calculateSize(context);
+            }
+
+            get["_initialized"] = true;
+            return render(context);
+          });
         });
   }
 
   Widget render(BuildContext context);
+
+  _calculateSize(BuildContext context) {
+    final parent =
+        (context.findRenderObject() as RenderBox).parent as RenderBox;
+
+    get["height"] = (get["height"] ?? 100.0) /
+        100.0 *
+        (parent.hasSize
+            ? parent.size.height
+            : MediaQuery.of(context).size.height);
+    get["width"] = (get["width"] ?? 100.0) /
+        100.0 *
+        (parent.hasSize
+            ? parent.size.width
+            : MediaQuery.of(context).size.width);
+  }
 }
